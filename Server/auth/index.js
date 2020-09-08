f92bfa89246373c711b5f94c0315576a449d8d73
albieduffy <albie.duffy@gmail.com>
2020-09-08 15:37:02 +0100

    install cookie-parser

--- Server/auth/index.js
+++ Server/auth/index.js
@@ -38,11 +38,34 @@ router.post('/signup', (req, res, next) => {
   }
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
   if(validateUser(req.body)) {
-    db.run()
-  } else {
+    db.run(getUserByEmail, [req.body.username])
+      .then(resp => {
+        if(resp.rows[0]) {
+          bcrypt.compare(req.body.password, resp.rows[0].password)
+            .then((result) => {
+              if(result) {
+                console.log(resp.rows[0])
+                res.cookie('user_id', resp.rows[0].userid, {
+                  httpOnly: true,
+                  signed: true,
+                  secure: true
+                })
+                res.json({
+                  message: 'Logged in! 🔓'
+                })
+              } else {
+                next(new Error('Wrong password'))
+              }
+            })
 
+        } else {
+          next(new Error('Username not found'))
+        }
+      })
+  } else {
+    next(new Error('Invalid Login'))
   }
 })
 
--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.use(cookieParser());
 
 const port = 3000;
 
